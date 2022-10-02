f1f50c2b13c3901a6790eea12b26bb3f456b325a
Erdem Ozgen <m.erdemozgen@gmail.com>
2022-10-02 21:54:31 +0300

    Add Bash and move to Folder

--- welcome.java
+++
@@ -1,5 +0,0 @@
-class welcome {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Data Engineer Roadmap!"); 
-    }
-}
---
+++ welcomes/welcome.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Welcome to Data Engineer Roadmap!"
